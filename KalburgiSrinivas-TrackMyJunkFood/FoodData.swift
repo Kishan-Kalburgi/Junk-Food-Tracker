f1bdac50c855fb9fb5caded8e81461c36997eaa5
student <s530468@ch140015m.nwmissouri.edu>
2018-03-06 19:20:41 -0600

    first day last

---
+++ KalburgiSrinivas-TrackMyJunkFood/FoodData.swift
@@ -0,0 +1,15 @@
+//
+//  FoodData.swift
+//  KalburgiSrinivas-TrackMyJunkFood
+//
+//  Created by Kalburgi Srinivas,Kishan on 3/6/18.
+//  Copyright © 2018 Kalburgi Srinivas,Kishan. All rights reserved.
+//
+
+import UIKit
+
+struct FoodData {
+    var Name: String
+    var Calories: Double
+    var Tally: Int
+}
