5ed315534768a0deff335fc73f73a620a9ac3c2a
KK Charli <kkcharli@KKs-Mac.local>
2018-03-08 00:15:48 -0600

    second day first commit

--- KalburgiSrinivas-TrackMyJunkFood/model.swift
+++ KalburgiSrinivas-TrackMyJunkFood/model.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+
+struct FoodData {
+    var Name: String
+    var Calories: Double
+    var Tally: Int
+}
+
+class FoodRecorder {
+    
+    var Foods: [FoodData]
+    
+    init() {
+        Foods = []
+    }
+    
+    func loadData() {
+        Foods.append(FoodData.init(Name: "Popcord", Calories: 106, Tally: 1))
+        Foods.append(FoodData.init(Name: "Coke", Calories: 140, Tally: 1))
+        Foods.append(FoodData.init(Name: "Potato", Calories: 152, Tally: 1))
+    }
+    
+    func increaseTally(forItem i:Int) {
+        Foods[i].Tally += 1
+    }
+    
+    func getCalories(forItime i:Int) -> Double {
+        //        will compute and return the
+        //        number of calories for item with index i. (Multiply the tally by the
+        //        calories)
+        return Double (Foods[i].Tally) * Foods[i].Calories
+    }
+    
+    func report(forItem i:Int) -> String {
+        return "The calories consumed for \(Foods[i].Name) is \(getCalories(forItime: i))."
+    }
+    
+    func totalCalories() -> Double {
+        //        sum of the calories over
+        //        all items
+        var totalCalories = 0.0
+        for i in 0 ... Foods.count-1 {
+            totalCalories += Foods[i].Calories
+        }
+        return totalCalories
+    }
+    
+    func combinedReport() -> String {
+        return "Total calories consumed \(totalCalories())."
+    }
+    
+    func reset() {
+        for i in 0 ... Foods.count - 1 {
+            Foods[i].Tally = 0
+        }
+    }
+    
+}
