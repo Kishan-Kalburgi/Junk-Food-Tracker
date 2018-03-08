a4dc704dc19c8138a5e5ceda0ce30395c29cc99a
KK Charli <kkcharli@KKs-Mac.local>
2018-03-08 00:15:26 -0600

    second day first commit

---
+++ KalburgiSrinivas-TrackMyJunkFood/model.swift
@@ -0,0 +1,9 @@
+//
+//  model.swift
+//  KalburgiSrinivas-TrackMyJunkFood
+//
+//  Created by KK Charli on 3/7/18.
+//  Copyright © 2018 Kalburgi Srinivas,Kishan. All rights reserved.
+//
+
+import Foundation
